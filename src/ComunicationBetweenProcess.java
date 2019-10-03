469e44734e90b3c979461bc84b16dd296f5f7cea
Marcos <marcosiesella@gmail.com>
2019-10-03 13:58:22 +0200

    Ejercicio 3 done

--- src/ComunicationBetweenProcess.java
+++ src/ComunicationBetweenProcess.java
@@ -6,17 +6,23 @@ import java.util.Arrays;
 
 // a) Se obtiene el texto que te sale cuando ejecutas el cmd porque hemos cogido la informacion del proceso hijo(cmd) y se la hemos enviado al proceso padre y lo hemos escrito por pantalla
 // b) Ha cambiado el simbolo que sustituía a la ó de Versión por una interrogación
-// c) 
+// c) Try catch puesto e hijo cerrado
 public class ComunicationBetweenProcess {
-	public static void main(String args[]) throws IOException {
-		Process process = new ProcessBuilder(args).start();
-		InputStream is = process.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);
-		String line;
-		System.out.println("Salida del proceso " + Arrays.toString(args) + ":");
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
+	public static void main(String args[]) {
+		try {
+			Process process = new ProcessBuilder(args).start();
+			InputStream is = process.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			BufferedReader br = new BufferedReader(isr);
+			String line;
+			System.out.println("Salida del proceso " + Arrays.toString(args) + ":");
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			is.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
 	}
 }
